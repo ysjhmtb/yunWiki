0ee70174928e976260357fc7a3d9ded1f6efd556
yun <yun@yunui-MacBook-Pro.local>
2019-10-16 15:28:52 +0900

    context attempt

--- Project/react-wiki/src/components/Wikiview/Contentview.js
+++ Project/react-wiki/src/components/Wikiview/Contentview.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Link } from 'react-router-dom';
 import MarkdownRenderer from 'react-markdown-renderer';
 import './Contentview.css';
+import { UserConsumer } from '../../contexts/userContext';
+import {Test} from '../../contexts/Test';
 
-const Contentview = (props)=>{
+
+const Contentview = (props) => {
+
     return (
-        <div> 
-            <div className="catagoryDiv">{props.category}</div> 
+        <div>
+            <div className="catagoryDiv">{props.category}</div>
             <div className="writingDiv"><Link to="/"> 작성하기 </Link></div>
 
-            <hr/>
+            <hr />
             <MarkdownRenderer markdown={props.content} />
+
+            {/* <Test/> */}
+        
+
         </div>
     );
 };
